1ed2301e2596ffcdcf320388b56950b0ff670df5
matt <matt@mailinator.com>
2016-09-05 22:29:28 +1000

    Log class tested

--- app/src/main/java/com/nasaimage/imageviewer/MainActivity.java
+++ app/src/main/java/com/nasaimage/imageviewer/MainActivity.java
@@ -3,6 +3,7 @@ package com.nasaimage.imageviewer;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
+        Log.i("tag","Test MSG");
+        Log.e("Error","Happened");
     }
 
     public void onRefresh(View v){
